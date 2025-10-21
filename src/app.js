569195220473b25d5f558f865fb817c84fd2dd2d
Sergio Bonaque Rubio <s.bonaquecontacto@gmail.com>
2025-10-21 20:19:40 +0000

    guardar

--- src/app.js
+++ src/app.js
@@ -9,3 +9,12 @@ window.onload = function() {
   //write your code here
   console.log("Hello Rigo from the console!");
 };
+
+let pronoun = ['the', 'our'];
+let adj = ['great', 'big'];
+let noun = ['jogger', 'racoon'];
+
+for (let i=0; i<=1; i++) {
+console.log(pronoun[i])
+
+}
